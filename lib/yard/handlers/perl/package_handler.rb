4becfbb9c278b650cf974071c471bc6e37b27929
Pieter Vande Bruggen <pvande@gmail.com>
2010-04-16 08:26:19 -0700

    Create (undocumented) ModuleObjects for each step in the heirarchy.
    This allows the Yard output to properly fold namespaces.

--- lib/yard/handlers/perl/package_handler.rb
+++ lib/yard/handlers/perl/package_handler.rb
@@ -2,6 +2,8 @@ class YARD::Handlers::Perl::PackageHandler < YARD::Handlers::Perl::Base
   handles Package
 
   process do
+    x = statement.namespace
+    register ModuleObject.new(x.namespace, x.name) until (x = P(x).parent).root?
     $__PACKAGE__ = register ClassObject.new(P(statement.namespace), statement.classname)
   end
 end
