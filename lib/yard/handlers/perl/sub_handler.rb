0d98bc1d4d5b78efd36b9a26b68f161e92796813
Pieter Vande Bruggen <pvande@gmail.com>
2010-04-19 09:24:34 -0700

    Adding basic support for automatically reading method signatures, and for automatically determining method scope from the name of the initial parameter.
    Parameter names are found by the first occurance of the pattern `my \((.*)\) = @_;`, including in a comment.
    
    Scope can be overridden with the @scope tag.

--- lib/yard/handlers/perl/sub_handler.rb
+++ lib/yard/handlers/perl/sub_handler.rb
@@ -7,13 +7,13 @@ class YARD::Handlers::Perl::SubHandler < YARD::Handlers::Perl::Base
       m.visibility = statement.visibility
     end
 
+    add_parameters(method.source, method)
+
     if method.has_tag?(:scope)
       case (scope = method.tag(:scope).text.downcase)
       when 'class', 'instance'
         method.scope = scope
-      when 'self'
-        method.scope = :instance
-      when 'dual', 'receiver', 'invocant', 'class_or_self'
+      when 'dual'
         class_method = method.dup
         class_method.scope = :class
         register class_method
@@ -21,5 +21,35 @@ class YARD::Handlers::Perl::SubHandler < YARD::Handlers::Perl::Base
         log.warn "Unrecognized @scope '#{scope}' for method #{statement.name}"
       end
     end
+  end
+
+  def add_parameters(code, method)
+    params = []
+    if code =~ /my\s+\((.*?)\)\s*=\s*@_\s*;/
+      params = $1.split(/\s*(?:,|=>)\s*/)
+
+      parse_method_scope(params.first, method)
+
+      method.parameters = params.map { |p| [ p ] }
+    end
+  end
+
+  private
+
+  def parse_method_scope(parameter, method)
+    return if method.has_tag?(:scope)
+
+    scope = case (parameter)
+    when '$self', '$instance'
+      'instance'
+    when '$class', '$package'
+      'class'
+    when '$receiver', '$invocant', '$class_or_self'
+      'dual'
+    else
+      return
+    end
+
+    method.docstring.add_tag(YARD::Tags::Tag.new(:scope, scope))
   end
 end
