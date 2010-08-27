72e90678931064db1600390bea0faf2740f88823
Pieter van de Bruggen <pvande@gmail.com>
2010-08-26 18:51:07 -0700

    Made-up namespace modules should not get a docstring.

--- lib/yard/handlers/perl/package_handler.rb
+++ lib/yard/handlers/perl/package_handler.rb
@@ -2,8 +2,15 @@ class YARD::Handlers::Perl::PackageHandler < YARD::Handlers::Perl::Base
   handles Package
 
   process do
+    docstring = statement.comments
     x = P(statement.namespace)
-    register ModuleObject.new(x.namespace, x.name) while x.is_a?(Proxy) || (P(x).parent.nil? && (x = P(x).parent).root?)
+    
+    statement.comments = ''
+    while x.is_a?(Proxy) || (P(x).parent.nil? && (x = P(x).parent).root?)
+      register ModuleObject.new(x.namespace, x.name)
+    end
+    
+    statement.comments = docstring
     $__PACKAGE__ = register ClassObject.new(P(statement.namespace), statement.classname)
   end
 end
