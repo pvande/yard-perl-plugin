b90615d422cee7cbac7d10fe406f0df92944ca60
Pieter van de Bruggen <pvande@gmail.com>
2010-08-26 18:01:48 -0700

    Fixing a namespace collision / resolution problem.  All of your packages should now properly appear!

--- lib/yard/handlers/perl/package_handler.rb
+++ lib/yard/handlers/perl/package_handler.rb
@@ -2,8 +2,8 @@ class YARD::Handlers::Perl::PackageHandler < YARD::Handlers::Perl::Base
   handles Package
 
   process do
-    x = statement.namespace
-    register ModuleObject.new(x.namespace, x.name) until P(x).parent.nil? || (x = P(x).parent).root?
+    x = P(statement.namespace)
+    register ModuleObject.new(x.namespace, x.name) while x.is_a?(Proxy) || (P(x).parent.nil? && (x = P(x).parent).root?)
     $__PACKAGE__ = register ClassObject.new(P(statement.namespace), statement.classname)
   end
 end
