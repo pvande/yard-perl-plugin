1bbf59f95a9cccc6fc265c29c092d323ebbe6597
Pieter van de Bruggen <pvande@gmail.com>
2010-08-29 16:24:42 -0700

    Setting up yard-perl-plugin.rb to autoload.

--- lib/yard-perl-plugin.rb
+++ lib/yard-perl-plugin.rb
@@ -1,31 +1,55 @@
-require File.dirname(__FILE__) + '/yard/parser/perl'
-require File.dirname(__FILE__) + '/yard/handlers/perl/base'
-require File.dirname(__FILE__) + '/yard/handlers/perl/package_handler'
-require File.dirname(__FILE__) + '/yard/handlers/perl/base_class_handler'
-require File.dirname(__FILE__) + '/yard/handlers/perl/sub_handler'
-require File.dirname(__FILE__) + '/yard/serializers/pod_serializer'
-require File.dirname(__FILE__) + '/yard/formatters/pod_formatter'
-require File.dirname(__FILE__) + '/yard/templates/helpers/html_helper'
-require File.dirname(__FILE__) + '/yard/templates/helpers/pod_helper'
+require 'textpow'
+
+# @private
+def __p(*path) File.join(File.dirname(__FILE__), 'yard', *path) end
 
 module YARD
-  module Handlers
-    Processor.register_handler_namespace :perl, Perl
-  end
-
   module Parser
-    SourceParser.register_parser_type :perl, Perl::PerlParser, %w[ pm pl ]
-
     module Perl
-      PerlSyntax = Textpow::SyntaxNode.load(File.join(File.dirname(__FILE__), '..', 'Perl.plist'))
+      PerlSyntax = Textpow::SyntaxNode.load(__p('..', '..', 'Perl.syntax'))
+      autoload :PerlParser, __p('parser', 'perl')
     end
+    
+    SourceParser.register_parser_type :perl, Perl::PerlParser, %w[ pm pl ]
   end
 
-  module Templates
-    Engine.register_template_path File.join(File.dirname(__FILE__), '..', 'templates')
+  module Handlers
+    module Perl
+      autoload :Base,             __p('handlers', 'perl', 'base')
+      autoload :PackageHandler,   __p('handlers', 'perl', 'package_handler')
+      autoload :BaseClassHandler, __p('handlers', 'perl', 'base_class_handler')
+      autoload :SubHandler,       __p('handlers', 'perl', 'sub_handler')
+    end
+    Processor.register_handler_namespace :perl, Perl
   end
 
   module Tags
     Library.define_tag "Method Scope", :scope
   end
+
+  module Serializers
+    autoload :PODSerializer, __p('serializers', 'pod_serializer')
+  end
+
+  module Formatters
+    autoload :PODFormatter, __p('formatters', 'pod_formatter')
+  end
+
+  module Templates
+    module Helpers
+      module HtmlHelper
+        html_helper = __p('templates', 'helpers', 'html_helper')
+        define_method :html_syntax_highlight_perl do |source|
+          require html_helper
+          html_syntax_highlight_perl(source)
+        end
+      end
+
+      autoload :PODHelper, __p('templates', 'helpers', 'pod_helper')
+    end
+
+    Engine.register_template_path __p('..', '..', 'templates')
+  end
 end
+
+undef __p
