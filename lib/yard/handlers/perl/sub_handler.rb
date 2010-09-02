2df342861cd7975419386a103c6c4f684b5b7726
Pieter van de Bruggen <pvande@gmail.com>
2010-09-02 11:14:22 -0700

    Correcting a scope inference mistake.

--- lib/yard/handlers/perl/sub_handler.rb
+++ lib/yard/handlers/perl/sub_handler.rb
@@ -10,10 +10,10 @@ class YARD::Handlers::Perl::SubHandler < YARD::Handlers::Perl::Base
       m.docstring  = statement.comments
 
       m.scope = case statement.parameters.first
-        when /^(self|instance)$/                   then :instance
-        when /^(class|package)$/                   then :class
-        when /^(receiver|invocant|class_or_self)$/ then :dual
-        else                                            :instance  # Not really...
+        when /\$(self|instance)$/                   then :instance
+        when /\$(class|package)$/                   then :class
+        when /\$(receiver|invocant|class_or_self)$/ then :dual
+        else                                             :instance  # Not really...
       end unless statement.parameters.empty?
 
       m.scope = m.tag(:scope).text.downcase.to_sym if m.has_tag?(:scope)
